616a4da86054f5b9d5269d4165ff065303f46294
Priya Nagda <pri1311@users.noreply.github.com>
2021-03-11 19:01:25 +0530

    base template with nav bar done

---
+++ website/static/js/nav.js
@@ -0,0 +1,25 @@
+const showMenu = (headerToggle, navbarId) =>{
+    const toggleBtn = document.getElementById(headerToggle),
+    nav = document.getElementById(navbarId)
+    
+    // Validate that variables exist
+    if(headerToggle && navbarId){
+        toggleBtn.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+
+            toggleBtn.classList.toggle('fa-times')
+        })
+    }
+}
+showMenu('nav-toggle','navbar')
+
+
+const linkColor = document.querySelectorAll('.nav-link')
+
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
+}
+
+linkColor.forEach(l => l.addEventListener('click',colorLink))
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return '<h1>hello</h1>'
+    return render_template('/views/chat.html')
