af01065d62ba09b9de7d94839e7698f0de2400b2
Priya Nagda <pri1311@users.noreply.github.com>
2021-03-20 22:16:20 +0530

    basic create workspace modal added

--- website/static/js/chat.js
+++ website/static/js/chat.js
@@ -2,3 +2,18 @@ window.onload = function() {
 var x = document.getElementById("chats").lastElementChild;
 x.focus();
 };
+
+var createWorkspace = document.getElementById('createWorkspace');
+var createWorkspaceModal = document.getElementById('createWorkspaceModal');
+var blur = document.getElementById('blur');
+var closeCreateWorkspace = document.getElementById('closeCreateWorkspace');
+
+createWorkspace.addEventListener("click", function(){
+    createWorkspaceModal.classList.toggle('createWorkspaceModalShow');
+    blur.classList.toggle('bluractive');
+});
+
+closeCreateWorkspace.addEventListener("click", function(){
+    createWorkspaceModal.classList.toggle('createWorkspaceModalShow');
+    blur.classList.toggle('bluractive');
+});
