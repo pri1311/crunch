f3b31b3a946b3f0feaa2f999bf5b645b69639c0c
jainam <j.dharod1238@gmail.com>
2021-03-22 16:20:26 +0530

    updated backend

--- website/auth.py
+++ website/auth.py
@@ -1,30 +1,28 @@
-from flask import Blueprint, render_template,request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_user, logout_user, login_required
 from . import db
 from .__init__ import User
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/')
-def main_page():
-    return render_template("/auth/login-register.html")
 
-@auth.route('/signup',methods=['POST'])
+@auth.route('/signup', methods=['POST'])
 def signup_post():
-    error=None
+    error = None
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
     if not password or not email or not name:
         error = "Invalid Credentials. Please try again."
-        return render_template("/auth/login-register.html",error=error)
+        return render_template("/auth/login-register.html", error=error)
 
     if User.query.filter_by(name=name).count() == 1:
         error = "Name already taken. Please try again."
-        return render_template("/auth/login-register.html",error=error)
+        return render_template("/auth/login-register.html", error=error)
 
     if User.query.filter_by(email=email).count() == 1:
         error = "Email already taken. Please try again."
-        return render_template("/auth/login-register.html",error=error)
+        return render_template("/auth/login-register.html", error=error)
 
     u = User()
     u.name = name
@@ -33,26 +31,32 @@ def signup_post():
 
     db.session.add(u)
     db.session.commit()
-    
-        
-    return render_template("/views/base.html",error=error)
+
+    return render_template("/views/base.html", error=error)
+
 
 @auth.route('/login', methods=['POST'])
 def login_post():
     error = None
     username = request.form.get('username')
     password = request.form.get('password')
-    print(username)
-    print(password)
+    remember = True if request.form.get('remember') else False
 
-    
     if not username or not password:
         error = "Missing Data"
-        return render_template("/auth/login-register.html",error=error)
+        return render_template("/auth/login-register.html", error=error)
 
     user = User.query.filter_by(name=username).first()
     if user is None or not user.check_password(password):
-        error="Please check your login details and try again."
-        return render_template("/auth/login-register.html",error=error)
-    
-    return render_template("/views/base.html",error=error)    
+        error = "Please check your login details and try again."
+        return render_template("/auth/login-register.html", error=error)
+
+    login_user(user, remember=remember)
+    return render_template("/views/base.html", error=error)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return render_template('/auth/login-register.html')
